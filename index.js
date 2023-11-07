0bb4b008671fa0008f67fe2818fdda2d5029e778
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-07 17:22:24 +0100

    Esercizio con 81 e 49 celle

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const play = document.getElementById("play");
 play.addEventListener("click", function(){
     const container = document.getElementById("container");
     container.style.display = "flex";
+    container.innerHTML = "";
+    const numeroCelle = document.getElementById("numeroCelle").value;
     // Crea celle
-    for(let i=1; i<=100; i++){
+    for(let i=1; i<=numeroCelle; i++){
         const cella = document.createElement("div");
         cella.classList.add("cella");
+        // Verifica quante celle creare
+        if(numeroCelle == "100"){
+            cella.classList.add("cella100");
+        }else if(numeroCelle == "81"){
+            cella.classList.add("cella81");
+        }else if(numeroCelle == "49"){
+            cella.classList.add("cella49");
+        }
         cella.textContent = i;
         // Btn cella click
         cella.addEventListener("click", function(){
