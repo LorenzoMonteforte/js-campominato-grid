33857900f9689f56d181db78347edb41cd41c22e
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-08 10:07:47 +0100

    Modifica codice JS in funzioni

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// Btn play click
-const play = document.getElementById("play");
-play.addEventListener("click", function(){
-    const container = document.getElementById("container");
-    container.style.display = "flex";
-    container.innerHTML = "";
-    const numeroCelle = document.getElementById("numeroCelle").value;
+// Funzione creaCelle
+function creaCelle(numeroCelle){
+    // Inizializza array celle
+    const celle = [];
     // Crea celle
     for(let i=1; i<=numeroCelle; i++){
         const cella = document.createElement("div");
@@ -17,6 +14,7 @@ play.addEventListener("click", function(){
         }else if(numeroCelle == "49"){
             cella.classList.add("cella49");
         }
+        // Aggiunge il numero alla cella
         cella.textContent = i;
         // Btn cella click
         cella.addEventListener("click", function(){
@@ -27,7 +25,29 @@ play.addEventListener("click", function(){
                 console.log("Hai cliccato sulla cella numero " + i);
             }
         })
-        // Mostra il contenuto in pagina
-        container.appendChild(cella);
+        // Aggiunge la cella creata all'array celle
+        celle.push(cella);
     }
+    return celle;
+}
+// Funzione mostra_celle
+function mostra_celle(numeroCelle, celle){
+    // Prende il container in cui inserire le celle
+    const container = document.getElementById("container");
+    container.style.display = "flex";
+    container.innerHTML = "";
+    // Mostra ciascuna cella dell'array celle
+    for(let i=0; i<numeroCelle; i++){
+        container.appendChild(celle[i]);
+    }
+}
+// Btn play click
+const play = document.getElementById("play");
+play.addEventListener("click", function(){
+    // Input numero celle
+    const numeroCelle = document.getElementById("numeroCelle").value;
+    // Invoca funzione creaCelle
+    const celle = creaCelle(numeroCelle);
+    // Invoca funzione mostraCelle
+    const mostraCelle = mostra_celle(numeroCelle, celle);
 })
